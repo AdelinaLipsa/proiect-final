eb0bb75ea568c33749a5c21b21c1b8a248bc23d3
AdelinaLipsa <adelina.lipsa@gmail.com>
2020-03-20 18:50:05 +0200

    website update

---
+++ personal_website/script.js
@@ -0,0 +1,35 @@
+$(window).scroll(function() {
+  $(".scrollDown").css("opacity", 1 - $(window).scrollTop() / 250);
+});
+
+$(window).scroll(function() {
+  var scrollPos = $(window).scrollTop();
+  navbar = $(".logo-text, .center");
+  navbarBg = $(".navbar");
+
+  if (scrollPos > $(window).height()) {
+    navbar.addClass("alt-color");
+    navbarBg.addClass("alt-color");
+  } else {
+    navbar.removeClass("alt-color");
+    navbarBg.removeClass("alt-color");
+  }
+});
+
+$(document).ready(function() {
+  $(".contact").click(function(e) {
+    $(".card").toggleClass("active");
+    $(".banner").toggleClass("active");
+    $(".photo").toggleClass("active");
+    $(".social-media-banner").toggleClass("active");
+    $(".email-form").toggleClass("active");
+    var buttonText = $("button.contact#main-button").text();
+    if (buttonText === "back") {
+      buttonText = "click to get in touch";
+      $("button.contact#main-button").text(buttonText);
+    } else {
+      buttonText = "back";
+      $("button.contact#main-button").text(buttonText);
+    }
+  });
+});
