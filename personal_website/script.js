8089fafc36763248feb08fe1ff48782e87df4b01
AdelinaLipsa <adelina.lipsa@gmail.com>
2020-03-27 15:13:51 +0200

    code cleanup

--- personal_website/script.js
+++ personal_website/script.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
       $("button.contact#main-button").text(buttonText);
     }
   });
+
   $("#projects > li").hover(
     function() {
       $(this)
@@ -48,28 +49,5 @@ $(document).ready(function() {
 
   $("#projects > li").click(function(e) {
     $(this).addClass("clicked");
-  });
-  function generateBalls() {
-    for (var i = 0; i < Math.floor(window.innerWidth / 20); i++) {
-      $(".gooey-animations").append(`
-      <div class="ball"></div>
-    `);
-      var colors = ["#28323B", "#ff4d5a"];
-      $(".ball")
-        .eq(i)
-        .css({
-          bottom: "0px",
-          left: Math.random() * window.innerWidth - 100,
-          "animation-delay": Math.random() * 5 + "s",
-          transform: "translateY(" + Math.random() * 10 + "px)",
-          "background-color": colors[i % 2]
-        });
-    }
-  }
-  generateBalls();
-
-  window.addEventListener("resize", function(e) {
-    $(".gooey-animations .ball").remove();
-    generateBalls();
   });
 });
