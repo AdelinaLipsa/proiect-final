0c7932a0fe85551eb1055cede013c60b23220436
AdelinaLipsa <adelina.lipsa@gmail.com>
2020-05-16 19:34:11 +0300

    "responsive "

--- personal_website/contactform.php
+++ personal_website/contactform.php
@@ -1,11 +1,9 @@
 <?php
 if(isset($_POST['email'])) {
  
-    // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "contact@adelinalipsa.com";
  
     function died($error) {
-        // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
         echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
@@ -14,20 +12,17 @@ if(isset($_POST['email'])) {
     }
  
  
-    // validation expected data exists
     if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['subject']) ||
         !isset($_POST['message'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
  
-    $first_name = $_POST['name']; // required
-    $email_from = $_POST['email']; // required
-    $subject = $_POST['subject']; // not required
-    $message = $_POST['message']; // required
+    $first_name = $_POST['name']; 
+    $email_from = $_POST['email']; 
+    $message = $_POST['message']; 
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -62,19 +57,18 @@ if(isset($_POST['email'])) {
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "subject: ".clean_string($subject)."\n";
     $email_message .= "message: ".clean_string($message)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $subject, $email_message, $headers);  
+@mail($email_to, $email_message, $headers);  
 ?>
  
 <!-- include your own success html here -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+Thank you for contacting me. I will be in touch with you very soon.
  
 <?php
  
